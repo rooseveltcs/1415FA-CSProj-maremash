73802940730ea4290a650dbd5505f0adbfa859da
mashmouse <marli@remash.com>
2014-10-08 07:15:04 -0700

    This version works, the only file that is not being uploaded is the Calendar/libs/android-support-v4.jar file.

--- Calendar/src/com/example/calendar/ViewCalendar.java
+++ Calendar/src/com/example/calendar/ViewCalendar.java
@@ -1,9 +1,8 @@
 package com.example.calendar;
 
-import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
